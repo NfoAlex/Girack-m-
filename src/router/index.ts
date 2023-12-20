b8b02a7d7d67cb2f79c0a5a59e83a4d6ca5f1886
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-20 14:43:15 +0900

    [change] - ディレクトリ構造の変更に伴うルーターの改善

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../components/MainView/HomeView/HomeView.vue'
+import HomeView from '../components/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
