e3ae9a8892c80257f597f975c3104fef638ce1a9
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-01-30 22:25:11 +0900

    [add] - パラメータを表示する例やってみた

---
+++ pages/channel/[id].vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { useRoute } from 'vue-router';
+
+export default {
+  setup() {
+    const route = useRoute();
+
+    return { route };
+  }
+}
+
+</script>
+
+<template>あなたは今 {{ route.params.id }} にいます</template>
---
+++ pages/channel/index.vue
@@ -0,0 +1,1 @@
+<template>ここページ</template>
