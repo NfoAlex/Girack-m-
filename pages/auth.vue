3b019055f7e4e102370734ae0243bb47bcd7ab18
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-01-31 17:58:26 +0900

    [add] - authページにて入力欄追加

--- pages/auth.vue
+++ pages/auth.vue
@@ -11,5 +11,47 @@ export default {
 </script>
 
 <template>
-  <p>asf</p>
+  <div class="d-flex" style="height:100vh; width:100vw;">
+    <div
+      class="instanceImage me-auto mr-n5"
+    >
+      ここが画像になる
+    </div>
+    <!-- ログイン/登録用パネル -->
+    <v-card class="panel rounded-e-0 d-flex flex-column justify-center">
+      <!-- 真ん中表示部分 -->
+      <div class="d-flex flex-column">
+        <p>ログイン</p>
+        <v-text-field variant="outlined"></v-text-field>
+        <p>パスワード</p>
+        <v-text-field variant="outlined"></v-text-field>
+
+        <v-btn class="mt-5" color="primary" block>ログイン!</v-btn>
+      </div>
+    </v-card>
+  </div>
 </template>
+
+<style scoped>
+
+.instanceImage {
+  /* デバッグ用ここから */
+  background-color: lightblue;
+  /* ここまで */
+
+  width:100%;
+
+  z-index: 50;
+}
+
+.panel {
+  width: 35vw;
+  min-width:400px;
+  max-width: 500px;
+
+  background-color: rgb(var(--v-theme-background));
+
+  z-index: 100;
+}
+
+</style>
