cf1b21579d5fa1738d09dcf9daf0308bd8c75ff7
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-01-31 14:09:55 +0900

    [remove] - tailwindの使用を消した

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["@/assets/styles/main.scss"],
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@pinia/nuxt"],
   vite: {
     css: {
       preprocessorOptions: {
