25a4250a3e54e7b13ed1f7eb2140286eea25028c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-01-31 17:21:45 +0900

    [add] - 認証ページ用vue追加

---
+++ pages/auth.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+export default {
+  setup() {
+    definePageMeta({
+      layout: 'plain' //レイアウトを何もないやつに設定
+    });
+  }
+}
+
+</script>
+
+<template>
+  <p>asf</p>
+</template>
