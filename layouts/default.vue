d60721868430941ca2bfa658948d0fb03f114217
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 14:27:47 +0900

    [remove] - 不要
    ついでにコメント

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,14 +10,7 @@ export default {
 
 <template>
   <div class="d-flex ma-0 pa-0" style="height:100vh; width:100vw;">
-    <!-- <span class="ma-3 d-flex flex-column">
-      <NuxtLink to="/"><v-btn color="primary" class="mx-1">トップ</v-btn></NuxtLink>
-      <NuxtLink to="/channel"><v-btn color="secondary" class="mx-1">チャンネルへ行ってみる</v-btn></NuxtLink>
-      <NuxtLink to="/channel/1234"><v-btn color="secondary" class="mx-1">適当なチャンネル</v-btn></NuxtLink>
-      <v-divider vertical></v-divider>
-      <NuxtLink to="/auth"><v-btn color="error" class="mx-1">認証ページ</v-btn></NuxtLink>
-    </span> -->
-
+    <!-- サイドバー -->
     <div class="sidebar d-flex flex-column pa-3">
       <span>
         <NuxtLink to="/">
@@ -63,6 +56,7 @@ export default {
       </span>
     </div>
 
+    <!-- メイン -->
     <div class="mainContainer rounded-xl align-self-auto">
       <slot />
     </div>
