4249ac952a6211699699ff02e243810f1b382304
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 20:29:00 +0900

    [add] - 設定ページにテーマページ追加

--- pages/settings/[page].vue
+++ pages/settings/[page].vue
@@ -11,15 +11,24 @@ export default {
 <template>
   <div class="pa-4 d-flex">
 
-    <!-- サイドバー -->
+    <!-- 左部分(サイドバー) -->
     <div class="d-flex flex-column" style="width:20%;">
         <p class="text-h6 pa-2">設定</p>
-        <v-card @click="" class="rounded-pill py-2 px-3">同期設定</v-card>
+        <v-card @click="$router.push('/settings/sync')" class="rounded-pill py-2 px-3">
+          同期設定
+        </v-card>
+        <v-card @click="$router.push('/settings/theme')" class="rounded-pill py-2 px-3">
+          テーマ
+        </v-card>
     </div>
 
+    <!-- 右部分 -->
     <div style="width:100%;">
       <SettingsSync
         v-if="$route.path==='/settings/sync'"
+      />
+      <SettingsTheme
+        v-if="$route.path==='/settings/theme'"
       />
     </div>
 
