b781bbd59b0f0c10e5e32131755814d107879de4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 20:00:36 +0900

    [add] - 同期ページの表示をできるように

--- pages/settings/[page].vue
+++ pages/settings/[page].vue
@@ -13,13 +13,18 @@ definePageMeta({
 
 <template>
   <div class="pa-4 d-flex">
+
     <!-- サイドバー -->
     <div class="d-flex flex-column" style="width:20%;">
         <p class="text-h6 pa-2">設定</p>
         <v-card @click="" class="rounded-pill py-2 px-3">同期設定</v-card>
     </div>
-    <div>
-      <SettingsSync />
+
+    <div style="width:100%;">
+      <SettingsSync
+        v-if="$route.path==='/settings/sync'"
+      />
     </div>
+
   </div>
 </template>
