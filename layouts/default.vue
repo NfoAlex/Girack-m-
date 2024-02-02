81e5a909f951f3b0719690e71b62ef40056c7133
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 15:24:12 +0900

    Main layout (#5)
    * [add] - メインのレイアウトにサイドバー追加
    
    * [add] - 背景色追加
    
    * [add] - <a>の色無効化
    
    * [add] - サイドバーにボタン追加
    
    * [change] - defaultレイアウトでの<script>移動
    
    * [add] - サイドバーのボタンを着色
    
    * [remove] - 不要
    
    ついでにコメント
    
    * [add] - サイドバーへ設定ボタンの追加
    
    * [add] - サイドバーの下部へディバイダ追加
    
    * [add] - コメント
    
    * [change] - scriptをsetupにしてrouteからrouterにした
    
    * [change] -ボタンのパス判別式をNuxtの$routerに
    
    * [fix] - 判別動いてないわ

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,101 @@
+<script setup lang="ts">
+
+import { onMounted } from "vue";
+const router = useRouter();
+
+onMounted(() => {
+  console.log("default :: mounted : router->", router);
+});
+
+</script>
+
 <template>
-  <div>
-    <span class="ma-3 d-flex">
-      <NuxtLink to="/"><v-btn color="primary" class="mx-1">トップ</v-btn></NuxtLink>
-      <NuxtLink to="/channel"><v-btn color="secondary" class="mx-1">チャンネルへ行ってみる</v-btn></NuxtLink>
-      <NuxtLink to="/channel/1234"><v-btn color="secondary" class="mx-1">適当なチャンネル</v-btn></NuxtLink>
-      <v-divider vertical></v-divider>
-      <NuxtLink to="/auth"><v-btn color="error" class="mx-1">認証ページ</v-btn></NuxtLink>
-    </span>
-    <div>
+  <div class="d-flex ma-0 pa-0" style="height:100vh; width:100vw;">
+    <!-- サイドバー -->
+    <div class="sidebar d-flex flex-column pa-3">
+      <!-- ホームボタン -->
+      <span>
+        <NuxtLink to="/">
+          <v-btn
+            icon=""
+            :variant="$route.path==='/'?'tonal':'text'"
+            :color="$route.path==='/'?'primary':null"
+            rounded="lg"
+            class="mt-2"
+          >
+            <v-icon size="large">mdi:mdi-home</v-icon>
+          </v-btn>
+          <p class="text-caption text-center">ホーム</p>
+        </NuxtLink>
+      </span>
+
+      <!-- チャンネルボタン -->
+      <span class="mt-4">
+        <NuxtLink to="/channel">
+          <v-btn
+            icon=""
+            :variant="$route.path==='/channel'?'tonal':'text'"
+            :color="$route.path==='/channel'?'primary':null"
+            rounded="lg"
+          >
+            <v-icon size="large">mdi:mdi-pound</v-icon>
+          </v-btn>
+          <p class="text-caption text-center">チャット</p>
+        </NuxtLink>
+      </span>
+
+      <!-- ------ ここから下 ----- -->
+
+      <v-divider class="mt-auto"></v-divider>
+
+      <!-- 設定ボタン -->
+      <span class="mt-1">
+        <NuxtLink to="/settings">
+          <v-btn
+            icon="mdi:mdi-cog"
+            variant="text"
+            rounded="lg"
+            class=""
+          >
+          </v-btn>
+          <p class="text-caption text-center">設定</p>
+        </NuxtLink>
+      </span>
+
+      <!-- FOR DEBUG :: ログインページへ -->
+      <span class="mt-3">
+        <NuxtLink to="/auth">
+          <v-btn
+            icon="mdi:mdi-lock"
+            variant="text"
+            rounded="lg"
+            class=""
+            color="error"
+          >
+          </v-btn>
+          <p class="text-caption text-center">認証</p>
+        </NuxtLink>
+      </span>
+    </div>
+
+    <!-- メイン -->
+    <div class="mainContainer rounded-xl align-self-auto">
       <slot />
     </div>
+
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<style scoped>
+
+.sidebar {
+  height: 100vh;
+}
+
+.mainContainer {
+  height:100vh;
+  width: 100%;
+  background: #fff;
+}
+
+</style>
