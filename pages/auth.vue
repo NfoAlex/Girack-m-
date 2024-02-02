878b735c60785239ee2c52cb42d56f3225901ae8
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 13:54:17 +0900

    [未完成] Auth window (#3)
    * [add] - 何もないレイアウト用vue追加
    
    * [add] - 認証ページ用vue追加
    
    * [add] - 認証ページ用ボタン追加
    
    * [change] - VCard用のデフォルト設定変更
    
    material3っぽく
    
    * [change] - VCard用のデフォルト設定変更
    
    * [change] - Vuetify用のデフォルト設定更新
    
    * [add] - authページにて入力欄追加
    
    * [change] - ちょっとだけ認証パネルのテキスト周り調整
    
    * [add] - 認証パネルのログイン用ボタンさサイズ調整
    
    * [add] - 認証パネルのログインボタンに基本的処理追加
    
    * [change] - 認証パネル内padding調整
    
    * [fix] - ボタンに対していらないpaddingをデフォルトとして設定していた
    
    * [add] - 認証パネルにログイン登録ボタン追加
    
    * [add] - ログインと登録用の別枠を追加
    
    * [add] - ログイン/登録ボタンで枠が切り替わるように
    
    * [change] - あっ
    
    * [change] - ログイン/登録ボタンの見た目変更
    
    * [add] - ボタンに色を追加
    
    * [add] - 認証画面での入力部分内部にアイコン追加
    
    * [add] - 認証画面にて登録用の暫定処理を追加
    
    * [add] - 認証画面での入力部分の変数リンク
    
    * [add] - 認証画面にてGirackタイトルの表示
    
    * [add] - 認証画面にてページ遷移するようにした
    
    import文無しで動かせるのNuxt怖すぎる
    
    * [remove] - 不要

---
+++ pages/auth.vue
@@ -0,0 +1,169 @@
+<script lang="ts">
+
+export default {
+  setup() {
+    definePageMeta({
+      layout: 'plain' //レイアウトを何もないやつに設定
+    });
+
+    const router = useRouter(); //ページ遷移用
+    return { router };
+  },
+
+  data() {
+    return {
+      authMode: "LOGIN" as string, // "LOGIN" | "REGISTER"
+      processingAuth: false as boolean, //ボタンの処理中表示用
+
+      //text-field用
+      username: "" as string, //ユーザー名
+      password: "" as string, //パスワード
+      invitecode: "" as string //招待コード
+    }
+  },
+
+  methods: {
+    //ログイン認証
+    login():void {
+      //処理中と設定
+      this.processingAuth = true;
+
+      //FOR DEBUG
+      setTimeout(() => {
+        this.processingAuth = false;
+        this.router.push("/"); //トップページへ移動
+      }, 1500);
+
+    },
+
+    //新規登録
+    register():void {
+      //処理中と設定
+      this.processingAuth = true;
+
+      //FOR DEBUG
+      setTimeout(() => {
+        this.processingAuth = false;
+      }, 1500);
+    }
+  }
+
+}
+
+</script>
+
+<template>
+  <div class="d-flex" style="height:100vh; width:100vw;">
+
+    <!-- カバー画像 -->
+    <div
+      class="instanceImage me-auto mr-n5"
+    >
+      ここが画像になる
+    </div>
+
+    <!-- ログイン/登録用パネル -->
+    <v-card class="panel pa-6 rounded-e-0 d-flex flex-column justify-center">
+      <!-- Girackタイトル -->
+      <p class="text-h4 text-center my-5">Girack</p>
+      <!-- ログイン/登録ボタン -->
+      <div class="d-flex justify-center py-4">
+        <v-btn
+          @click="authMode='LOGIN'"
+          class="mx-1"
+          size="large"
+          :color="authMode==='LOGIN'?'primary':null"
+          :variant="authMode==='LOGIN'?'tonal':'text'"
+        >ログイン</v-btn>
+        <v-btn
+          @click="authMode='REGISTER'"
+          class="mx-1"
+          size="large"
+          :color="authMode==='REGISTER'?'primary':null"
+          :variant="authMode==='REGISTER'?'tonal':'text'"
+        >新規登録</v-btn>
+      </div>
+      <!-- 真ん中表示部分 -->
+        <!-- ログイン部分 -->
+      <div v-if="authMode==='LOGIN'" class="d-flex flex-column">
+        <p class="my-2">ユーザー名</p>
+        <v-text-field
+          v-model="username"
+          variant="outlined"
+          prepend-inner-icon="mdi:mdi-account"
+        ></v-text-field>
+        <p class="my-2">パスワード</p>
+        <v-text-field
+          v-model="password"
+          variant="outlined"
+          prepend-inner-icon="mdi:mdi-key"
+          type="password"
+        ></v-text-field>
+
+        <!-- ログインボタン -->
+        <v-btn
+          @click="login"
+          :loading="processingAuth"
+          class="mt-5"
+          size="large"
+          color="primary"
+          block
+        >
+          ログイン!
+        </v-btn>
+      </div>
+        <!-- 登録部分 -->
+      <div v-else class="d-flex flex-column">
+        <p class="my-2">ユーザー名</p>
+        <v-text-field
+          v-model="username"
+          variant="outlined"
+          prepend-inner-icon="mdi:mdi-account"
+        ></v-text-field>
+        <p class="my-2">招待コード</p>
+        <v-text-field
+          v-model="invitecode"
+          variant="outlined"
+          prepend-inner-icon="mdi:mdi-email"
+        ></v-text-field>
+
+        <!-- 登録ボタン -->
+        <v-btn
+          @click="register"
+          :loading="processingAuth"
+          class="mt-5"
+          size="large"
+          color="primary"
+          block
+        >
+          登録する
+        </v-btn>
+      </div>
+    </v-card>
+
+  </div>
+</template>
+
+<style scoped>
+
+.instanceImage {
+  /* デバッグ用ここから */
+  background-color: lightblue;
+  /* ここまで */
+
+  width:100%;
+
+  z-index: 50;
+}
+
+.panel {
+  width: 35vw;
+  min-width:400px;
+  max-width: 500px;
+
+  background-color: rgb(var(--v-theme-background));
+
+  z-index: 100;
+}
+
+</style>
