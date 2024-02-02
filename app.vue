363b06a75d9d373c3aac5c61a049dd1c674107e8
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 14:20:08 +0900

    [add] - <a>の色無効化

--- app.vue
+++ app.vue
@@ -8,6 +8,17 @@
 
 <style>
 
+a {
+  text-decoration: none;
+  transition: 0.4s;
+  color: rgb(var(--v-theme-on-surface));
+}
+
+a:visited {
+  text-decoration: none;
+  color: rgb(var(--v-theme-on-surface));
+}
+
 html {
   background-color: #efefef
 }
