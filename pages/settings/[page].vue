f1688478c35ea4e1450c3e686574314feb828ca5
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 19:51:52 +0900

    [add] - 設定ページのルーティング整備

---
+++ pages/settings/[page].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+definePageMeta({
+  middleware: [
+    function (to, from) {
+      console.log("settings[page] :: definePageMeta : to->", to);
+    },
+    'settings',
+  ],
+});
+
+</script>
+
+<template>
+  <div class="pa-4 d-flex">
+    <!-- サイドバー -->
+    <div class="d-flex flex-column" style="width:20%;">
+        <p class="text-h6 pa-2">設定</p>
+        <v-card @click="" class="rounded-pill py-2 px-3">同期設定</v-card>
+    </div>
+    <div>
+      <SettingsSync />
+    </div>
+  </div>
+</template>
---
+++ pages/settings/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+//同期ページへ移動させるだけ
+
+const router = useRouter();
+
+onMounted(() => {
+  router.push("/settings/sync"); //同期ページへ移動
+})
+
+</script>
