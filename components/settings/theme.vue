43401bc15364f75dd5c8c2a42429471e25ce906c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-02 20:28:20 +0900

    [add] - テーマ設定用ページ追加
    テーマのトグルいけます

---
+++ components/settings/theme.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const theme = useTheme();
+onMounted(() => {
+  console.log("theme(settings) :: onMounted : theme->", theme);
+});
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.name.value==="dark"?"light":"dark";
+  console.log("theme(settings) :: toggleTheme : theme.name->", theme.global.name.value);
+}
+
+</script>
+
+<template>
+  ここがテーマ
+  <v-btn @click="toggleTheme">テーマトグル</v-btn>
+
+</template>
