533cb13ec1c9adab11f2373a67cc0a0fed36b346
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-09 02:42:31 +0900

    [fix] - Serverinfo用のstoreインポートタイミングを改善

--- socketHandlers/InfoServer.ts
+++ socketHandlers/InfoServer.ts
@@ -1,13 +1,13 @@
 //サーバー情報受け取り
 
 import { Socket } from "socket.io-client"; //クラス識別用
-import { storeServerinfo } from "../stores/serverinfo";
-const Serverinfo = storeServerinfo(); //piniaからインポート
+import { useServerinfo } from "../stores/serverinfo";
 
 export default function infoServer(socket: Socket): void {
   //サーバー(インスタンス)情報
   socket.on("infoServer", (dat) => {
     console.log("infoServer :: dat->", dat);
+    const Serverinfo = useServerinfo(); //piniaからインポート
     Serverinfo.updateInfo(dat); //サーバー情報へ適用
   });
 }
