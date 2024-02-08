1a6769c719bff28254a4e67205bd24bcb26e0cf1
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-09 02:47:43 +0900

    [fix] - フロントエンド側でpiniaからデータを持ってきた図
    まだ完全にno active piniaエラーが解消しきってない

--- components/settings/sync.vue
+++ components/settings/sync.vue
@@ -1,3 +1,12 @@
-<script></script>
+<script setup lang="ts">
+import { useServerinfo } from "../../stores/serverinfo";
+const Serverinfo:any = useServerinfo();
+</script>
 
-<template>ここで同期</template>
+<template>
+  <p>ここで同期</p>
+  <m-card>
+    <p>カウンタ : {{ Serverinfo.count }}</p>
+    <p>Serverinfo : {{ Serverinfo.serverinfo }}</p>
+  </m-card>
+</template>
