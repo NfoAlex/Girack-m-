585e54adfc42b29a79ac0937eb218a1de3c4b89e
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-09 20:57:11 +0900

    [add] - トップページにてpiniaでデータを表示するように

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import { useServerinfo } from "../stores/serverinfo";
+const Serverinfo:any = useServerinfo();
+</script>
+
 <template>
   <div class="pa-5">
     <div class="">HelloWorld</div>
+    <m-card>
+      <p>カウンタ : {{ Serverinfo.count }}</p>
+      <p>Serverinfo : {{ Serverinfo.serverinfo }}</p>
+    </m-card>
   </div>
 </template>
