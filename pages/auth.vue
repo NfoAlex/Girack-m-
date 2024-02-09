cf940ce8a292cbe1c5a335ace6cbfef4e1255454
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-09 22:19:34 +0900

    [add] - 結果がエラー時用のアラート追加

--- pages/auth.vue
+++ pages/auth.vue
@@ -101,6 +101,16 @@ export default {
     <v-card class="panel pa-6 rounded-e-0 d-flex flex-column justify-center">
       <!-- Girackタイトル -->
       <p class="text-h4 text-center my-5">Girack</p>
+      <div>
+        <v-alert
+          v-if="result==='FAILED'"
+          type="error"
+          class="flex-shrink-1 flex-grow-0"
+          style="min-height:max-content;"
+        >
+          <v-alert-title>認証に失敗しました</v-alert-title>
+        </v-alert>
+      </div>
       <!-- ログイン/登録ボタン -->
       <div class="d-flex justify-center py-4">
         <v-btn
