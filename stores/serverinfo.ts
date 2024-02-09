3fd945ae2e6898930755c9eb7196a333dd1c137d
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-09 22:31:35 +0900

    [add] - Serverinfoのホルダー追加

--- stores/serverinfo.ts
+++ stores/serverinfo.ts
@@ -5,7 +5,34 @@ import type { Serverinfo } from "~/types/serverInfo";
 export const useServerinfo = defineStore("serverinfo", {
   state: () => {
     return {
-      serverinfo: {} as Serverinfo,
+      serverinfo: {
+        servername: "Girack-m-",
+        registration: {
+          available: false,
+          invite: {
+            inviteOnly: false,
+          },
+        },
+        config: {
+          PROFILE: {
+            PROFILE_ICON_MAXSIZE: "128",
+            PROFILE_USERNAME_MAXLENGTH: "32",
+          },
+          CHANNEL: {
+            CHANNEL_DEFAULT_REGISTERANNOUNCE: "0001",
+            CHANNEL_DEFAULT_JOINONREGISTER: ["0001"],
+            CHANNEL_CREATE_AVAILABLE: true,
+            CHANNEL_DELETE_AVAILABLEFORMEMBER: true,
+            CHANNEL_PRIVATIZE_AVAILABLEFORMEMBER: true,
+          },
+          MESSAGE: {
+            MESSAGE_PIN_ROLE: "Admin",
+            MESSAGE_TXT_MAXLENGTH: "500",
+            MESSAGE_FILE_MAXSIZE: "5e7",
+          },
+        },
+        serverVersion: "alpha_20230301",
+      } as Serverinfo,
       count: 0 as number
     }
   },
