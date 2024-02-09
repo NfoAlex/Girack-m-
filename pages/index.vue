c553c3679d5de21e1b03efe546bc2831d1e43c7d
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-09 23:05:05 +0900

    [add] - ホーム部分にpiniaのデータ表示するように

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { useServerinfo } from "../stores/serverinfo";
+import { useMyUserinfo } from "../stores/userinfo";
 const Serverinfo:any = useServerinfo();
+const MyUserinfo:any = useMyUserinfo();
 </script>
 
 <template>
   <div class="pa-5">
-    <div class="">HelloWorld</div>
+    <div class="text-h4 my-3">ここがホーム</div>
+    <p>piniaデータ : </p>
     <m-card>
       <p>カウンタ : {{ Serverinfo.count }}</p>
-      <p>Serverinfo : {{ Serverinfo.serverinfo }}</p>
+      <p>Serverinfo : </p>
+      <m-card variant="outlined">
+        {{ Serverinfo.serverinfo }}
+      </m-card>
+      <p>MyUserinfo : </p>
+      <m-card variant="outlined">
+        {{ MyUserinfo.MyUserinfo }}
+      </m-card>
     </m-card>
   </div>
 </template>
