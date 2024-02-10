65e67dc4f2e0f6cf678827c6db021e7a51a22783
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-10 21:08:50 +0900

    [add] - ユーザー情報import

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { useMyUserinfo } from "../stores/userinfo";
+const { getMyUserinfo } = storeToRefs(useMyUserinfo());
+</script>
+
 <template>
   <div class="pa-4">
     <p>
