3c4fd946ea6ea1fa76887958cde9e04aada99e73
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-19 18:18:09 +0900

    [add] - サーバー情報Socketハンドラに型指定

--- socketHandlers/InfoServer.ts
+++ socketHandlers/InfoServer.ts
@@ -2,10 +2,14 @@
 
 import { Socket } from "socket.io-client"; //クラス識別用
 import { useServerinfo } from "../stores/serverinfo";
+import type { Serverinfo } from "~/types/serverInfo";
 
 export default function infoServer(socket: Socket): void {
   //サーバー(インスタンス)情報
-  socket.on("RESULTfetchServerInfoLimited", (dat) => {
+  socket.on("RESULTfetchServerInfoLimited", (dat:{
+    result: string,
+    data: Serverinfo
+  }) => {
     console.log("infoServer :: dat->", dat);
     const Serverinfo = useServerinfo(); //piniaからインポート
     Serverinfo.updateInfo(dat.data); //サーバー情報へ適用
