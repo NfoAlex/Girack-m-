3c4d29cb28aea2a3503bc8aa6b0d41b35adbd155
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-19 18:15:14 +0900

    [change] - バックエンドの変数型更新に合わせてサーバー情報の型更新

--- types/serverInfo.ts
+++ types/serverInfo.ts
@@ -1,28 +1,23 @@
 export interface Serverinfo {
-  servername: string;
+  servername: string,
   registration: {
-    available: boolean;
+    available: boolean,
     invite: {
-      inviteOnly: boolean;
-    };
-  };
+      inviteOnly: boolean,
+    },
+  },
   config: {
     PROFILE: {
-      PROFILE_ICON_MAXSIZE: string;
-      PROFILE_USERNAME_MAXLENGTH: string;
-    };
+      iconMaxSize: number,
+      usernameMaxLength: number
+    },
     CHANNEL: {
-      CHANNEL_DEFAULT_REGISTERANNOUNCE: string;
-      CHANNEL_DEFAULT_JOINONREGISTER: string[];
-      CHANNEL_CREATE_AVAILABLE: boolean;
-      CHANNEL_DELETE_AVAILABLEFORMEMBER: boolean;
-      CHANNEL_PRIVATIZE_AVAILABLEFORMEMBER: boolean;
-    };
+      channelIdAnnounceRegistration: string,
+      defaultJoinOnRegister: string[],
+    },
     MESSAGE: {
-      MESSAGE_PIN_ROLE: string;
-      MESSAGE_TXT_MAXLENGTH: string;
-      MESSAGE_FILE_MAXSIZE: string;
-    };
-  };
-  serverVersion: string;
+      TxtMaxLength: number,
+      FileMaxSize: number,
+    },
+  }
 }
