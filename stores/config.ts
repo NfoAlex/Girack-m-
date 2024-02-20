b68948767de53fddf2054804cad2cf9f22e0aaad
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-20 12:11:42 +0900

    [add] - 設定データ用のStore追加

---
+++ stores/config.ts
@@ -0,0 +1,40 @@
+//サーバー情報保存
+import { defineStore } from "pinia";
+import type Config from "~/types/config";
+
+export const useMyUserinfo = defineStore("myuserinfo", {
+  state: () =>
+  ({
+    _Config: {
+      notification: {
+        enabled: true,
+        notifyAllMessages: false,
+        notifyMention: true
+      },
+      theme: "dark",
+      channel: {
+        displayRole: true,
+        displayAttatchmentSizeLimit: 1e7,
+      },
+      sidebar: {
+        ReadAllButtonEnabled: true,
+        ReadAllButtonByShiftKey: true
+      }
+    },
+  } as {
+    _Config: Config;
+  }),
+
+  getters: {
+    getMyUserinfo: (state) => {
+      return state._Config;
+    },
+  },
+  
+  actions: {
+    updateConfig(data:Config) {
+      this._Config = data;
+    },
+  }
+});
+
