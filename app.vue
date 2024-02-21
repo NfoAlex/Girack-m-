f540f9cfc2b1dbfb8be6bfc79f3681699c0040c2
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-21 18:34:23 +0900

    [add] - 設定をwatchしてテーマを更新するように

--- app.vue
+++ app.vue
@@ -1,8 +1,18 @@
 <script setup>
 import { loadSocket } from "../socketHandlers/socketInit";
+import { useConfig } from "../stores/config";
 import { socket } from './socketHandlers/socketInit';
 //サービスの準備ができてからSocketの設定開始
 onNuxtReady(() => { loadSocket(); })
+
+//設定の値監視してテーマを切り替え
+const { getConfig } = storeToRefs(useConfig());
+//テーマ切り替え用
+const theme = useTheme();
+//監視開始
+watch(getConfig, async () => {
+  theme.global.name.value = getConfig.value.theme; //テーマを設定で設定されているものへ設定
+});
 </script>
 
 <template>
