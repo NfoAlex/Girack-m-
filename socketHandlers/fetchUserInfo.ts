e4a729b50c26b1cb1255638cd0a5d922680492f5
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-24 15:59:26 +0900

    [change] - ファイル名変更

--- socketHandlers/fetchUserInfo.ts
+++ socketHandlers/fetchUserInfo.ts
@@ -4,7 +4,7 @@ import { Socket } from "socket.io-client"; //クラス識別用
 import { useMyUserinfo } from "../stores/userinfo";
 import type { MyUserinfo } from "../types/user";
 
-export default function infoUser(socket:Socket):void {
+export default function fetchUserInfo(socket:Socket):void {
   //プロフィール情報の受け取り
   socket.on("RESULT::fetchUserInfo", (dat:{result:string, data:MyUserinfo}) => {
     console.log("fetchUserInfo :: socketon(infoUser) : dat->", dat);
