3685373c7a31eb8d425ebf78517e8b95d28f0bc6
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-27 00:56:18 +0900

    [add] - テーマ設定用のスイッチ追加

--- pages/settings/theme.vue
+++ pages/settings/theme.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+import { useConfig } from '~/stores/config';
+
+const { getConfig } = storeToRefs(useConfig());
+
+/**
+ * data
+ */
+const themeSwitchDark = ref<boolean>(true);
+
 //テーマ切り替え用
 const theme = useTheme();
 
@@ -11,14 +20,24 @@ const toggleTheme = () => {
     theme.global.name.value
   );
 };
+
+//テーマ設定に応じてスイッチの値設定
+onMounted(() => {
+  if (getConfig.value.theme === "dark") themeSwitchDark.value = true;
+});
 </script>
 
 <template>
   <div>
     <NuxtLayout name="settings">
-      <div>
-        <p>ここがテーマ</p>
-        <m-btn @click="toggleTheme" color="primary">テーマトグルボタン</m-btn>
+      <p class="text-h6 mb-6">テーマ</p>
+      <div class="d-flex justify-center mx-auto">
+        <v-switch
+          @click="toggleTheme"
+          v-model="themeSwitchDark"
+          prepend-icon="mdi:mdi-weather-sunny"
+          append-icon="mdi:mdi-weather-night"
+        />
       </div>
     </NuxtLayout>
   </div>
