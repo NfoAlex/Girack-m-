e4bc6bd0b72efeea6a43e357e754f08c4f720646
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-27 00:29:29 +0900

    [add] - サイドバー設定の設定項目整備

--- pages/settings/sidebar.vue
+++ pages/settings/sidebar.vue
@@ -1,7 +1,25 @@
+<script setup lang="ts">
+import { useConfig } from '~/stores/config';
+
+const { getConfig } = storeToRefs(useConfig());
+</script>
+
+
 <template>
   <div>
     <NuxtLayout name="settings">
-      ここがサイドバー
+      <p class="text-h6 mb-6">チャンネル</p>
+      <v-switch
+        v-model="getConfig.sidebar.ReadAllButtonEnabled"
+        label="すべて既読に設定するボタンを有効化"
+      />
+      <v-switch
+        v-model="getConfig.sidebar.ReadAllButtonByShiftKey"
+        :disabled="!getConfig.sidebar.ReadAllButtonEnabled"
+        label="Shiftキーで全既読ボタンを表示する"
+        class="ml-auto"
+        style="width:calc(100% - 24px);"
+      />
     </NuxtLayout>
   </div>
 </template>
