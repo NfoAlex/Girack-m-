264712c2db1a9e993fb3f03ed384ceefe7812f68
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-27 00:24:21 +0900

    [add] - 画像表示サイズの設定追加

--- pages/settings/channel.vue
+++ pages/settings/channel.vue
@@ -9,6 +9,9 @@ const { getConfig } = storeToRefs(useConfig());
     <NuxtLayout name="settings">
       <p class="text-h6 mb-6">チャンネル</p>
       <v-switch v-model="getConfig.channel.displayRole" label="メッセージ送信者名の横にロールを表示する" class="ml-auto" />
+
+      <p class="text-medium-emphasis">プレビューとして表示する最高画像容量</p>
+      <v-text-field v-model="getConfig.channel.displayAttatchmentSizeLimit" rouned="xl"></v-text-field>
     </NuxtLayout>
   </div>
 </template>
