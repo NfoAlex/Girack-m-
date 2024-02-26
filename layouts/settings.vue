ef1fd825c0dbba69c7485bf55e9b509684c2ce45
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-26 23:38:18 +0900

    [add] - 設定ページ用のサイドバーへカテゴリ、ボタン追加

--- layouts/settings.vue
+++ layouts/settings.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="pa-4 d-flex">
+  <div class="py-4 d-flex">
 
     <!-- 左部分(サイドバー) -->
     <div class="d-flex flex-column" style="width:30%; max-width:250px;">
-        <p class="text-h5 pa-2">設定</p>
+      <p class="text-h5 py-4 px-8">設定</p>
+
+      <!-- 全般 -->
+      <span class="pl-2">
         <NuxtLink to="/settings/sync">
           <v-card
             class="rounded-pill py-2 px-3"
@@ -24,6 +27,26 @@
             テーマ
           </v-card>
         </NuxtLink>
+      </span>
+
+      <!-- UIカテゴリ -->
+      <span class="pt-5 pb-2">
+        <p class="pl-4">UI</p>
+        <v-divider></v-divider>
+      </span>
+      <!-- 全般 -->
+      <span class="pl-2">
+        <NuxtLink to="/settings/channel">
+          <v-card
+            class="rounded-pill py-2 px-3"
+            :variant="$route.path==='/settings/channel'?'tonal':'text'"
+            :ripple="false"
+          >
+            <v-icon>mdi:mdi-sync</v-icon>
+            チャンネル
+          </v-card>
+        </NuxtLink>
+      </span>
     </div>
 
     <!-- 右部分 -->
