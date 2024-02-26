f52551a67da575e1303b22ceeaf90eebffa50342
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-27 00:19:47 +0900

    [add] - チャンネル設定ページにてスイッチ追加してみた

--- pages/settings/channel.vue
+++ pages/settings/channel.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import { useConfig } from '~/stores/config';
+
+const { getConfig } = storeToRefs(useConfig());
+</script>
+
 <template>
   <div>
     <NuxtLayout name="settings">
-      チャンネル設定系
+      <p class="text-h6 mb-6">チャンネル</p>
+      <v-switch v-model="getConfig.channel.displayRole" label="メッセージ送信者名の横にロールを表示する" class="ml-auto" />
     </NuxtLayout>
   </div>
 </template>
