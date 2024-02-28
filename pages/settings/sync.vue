d8396364ab7172f36595028f28829b0bb4181ae4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-28 23:22:33 +0900

    [change] - 同期設定をONにしたときに上書きするかどうか確認するように

--- pages/settings/sync.vue
+++ pages/settings/sync.vue
@@ -9,22 +9,34 @@ const Serverinfo = useServerinfo();
  * data
  */
 const syncSwitch = ref<boolean>(true);
-
-//同期スイッチを監視
-watch(syncSwitch, () => {
-  //同期スイッチの値を設定Storeの同期設定へ適用
-  useConfig().updateConfigSyncStatus(syncSwitch.value);
-  //同期スイッチの値をlocalStorageへ書き込み
-  setConfigLocalSync(syncSwitch.value);
-});
+const displayConfirmSync = ref<boolean>(false); //同期設定の切り替え確認ダイアログ
 
 onMounted(() => {
   //同期設定をスイッチとして表示するため変数へ
   syncSwitch.value = getConfigSyncStatus.value;
-})
+
+  //同期設定を変数へ格納してから同期スイッチを監視
+  watch(syncSwitch, () => {
+    //オンにするのなら現在の設定をどうするか確認する
+    if (syncSwitch.value === true) {
+      //ダイアログ表示
+      displayConfirmSync.value = true;
+    } else {
+      //同期スイッチの値を設定Storeの同期設定へ適用
+      useConfig().updateConfigSyncStatus(false);
+      //falseをlocalStorageへ書き込み
+      setConfigLocalSync(false);
+    }
+  });
+});
 </script>
 
 <template>
+  <!-- 同期設定をオンにする際の上書き確認 -->
+  <SyncConfirm
+    v-model="displayConfirmSync"
+    @closeDialog="()=>{ displayConfirmSync=false; }"
+  />
   <div>
     <NuxtLayout name="settings">
       <p class="text-h6 mb-6">同期設定</p>
