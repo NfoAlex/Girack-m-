060869e17f1d4e705ef7688bd450b5f47fd510d3
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-03 18:20:33 +0900

    [add] - チャンネルリストを取得してraw表示できるように

--- pages/browser.vue
+++ pages/browser.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
 import { socket } from '~/socketHandlers/socketInit';
+import { useMyUserinfo } from '~/stores/userinfo';
+import type { channel } from '~/types/channel';
+
+const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+
+/**
+ * data
+ */
+const channelList = ref<channel[]>(); //チャンネル情報格納用
+
+/**
+ * チャンネルリストを受信
+ * @param dat
+ */
+const SOCKETRfetchChannelList = (dat:{result:string, data:channel[]}) => {
+  console.log("auth :: SOCKETRfetchChannelList : dat->", dat);
+  channelList.value = dat.data;
+}
+
+onMounted(() => {
+  socket.on("RESULT::fetchChannelList", SOCKETRfetchChannelList);
+
+  socket.emit("fetchChannelList", {
+    RequestSender: {
+      userId: getMyUserinfo.value.userId,
+      sessionId: getSessionId.value
+    }
+  });
+});
+
+onUnmounted(() => {
+  socket.off("RESULT::fetchChannelList", SOCKETRfetchChannelList);
+})
 </script>
 
 <template>
   <div class="pa-5">
     ここがチャンネルブラウザ
+    {{ channelList }}
   </div>
 </template>
