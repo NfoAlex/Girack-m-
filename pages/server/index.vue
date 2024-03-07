7f3f7aa9e0c0fd30311c6250e69502fce5c1d409
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-07 18:53:07 +0900

    [add] - サーバー情報をクローンして編集できるようにしてWatch処理追加

--- pages/server/index.vue
+++ pages/server/index.vue
@@ -1,8 +1,54 @@
 <script setup lang="ts">
 import { socket } from '~/socketHandlers/socketInit';
 import { useServerinfo } from '~/stores/serverinfo';
+import type { Serverinfo } from '~/types/serverInfo';
 
 const { getServerinfo } = storeToRefs(useServerinfo());
+
+/**
+ * data
+ */
+//自由に編集できるように
+const ServerinfoCloned = ref<Serverinfo>({
+  servername: '',
+  registration: {
+    available: false,
+    invite: {
+      inviteOnly: false
+    }
+  },
+  config: {
+    PROFILE: {
+      iconMaxSize: 0,
+      usernameMaxLength: 0
+    },
+    CHANNEL: {
+      channelIdAnnounceRegistration: '',
+      defaultJoinOnRegister: []
+    },
+    MESSAGE: {
+      TxtMaxLength: 0,
+      FileMaxSize: 0
+    }
+  }
+});
+const ServerinfoEdited = ref<boolean>(false);
+
+onMounted(() => {
+  //サーバー情報をクローン
+  ServerinfoCloned.value = structuredClone(toRaw(getServerinfo.value));
+
+  //クローンしたサーバー情報の編集を監視
+  watch(ServerinfoCloned.value, () => {
+    console.log("server(index) :: watch(mounted) : 変更検知");
+    //console.log("server(index) :: watch(mounted) : data->", JSON.stringify(ServerinfoCloned.value), JSON.stringify(getServerinfo.value));
+    if (JSON.stringify(ServerinfoCloned.value) === JSON.stringify(getServerinfo.value)) {
+      ServerinfoEdited.value = false;
+    } else {
+      ServerinfoEdited.value = true;
+    }
+  });
+})
 </script>
 
 <template>
@@ -14,20 +60,20 @@ const { getServerinfo } = storeToRefs(useServerinfo());
         <p class="text-h6">チャンネル</p>
         <m-card>
           <p>
-            新規登録者を通知するチャンネル : {{ getServerinfo.config.CHANNEL.channelIdAnnounceRegistration }}
+            新規登録者を通知するチャンネル : {{ ServerinfoCloned.config.CHANNEL.channelIdAnnounceRegistration }}
           </p>
           <p>
-            最初に参加するチャンネル : {{ getServerinfo.config.CHANNEL.defaultJoinOnRegister }}
+            最初に参加するチャンネル : {{ ServerinfoCloned.config.CHANNEL.defaultJoinOnRegister }}
           </p>
         </m-card>
 
         <p class="mt-3 text-h6">メッセージ</p>
         <m-card>
           <p>
-            メッセージの最大文字数 : {{ getServerinfo.config.MESSAGE.TxtMaxLength }}
+            メッセージの最大文字数 : {{ ServerinfoCloned.config.MESSAGE.TxtMaxLength }}
           </p>
           <p>
-            ユーザーが添付できるファイルの最大サイズ : {{ getServerinfo.config.MESSAGE.FileMaxSize }}
+            ユーザーが添付できるファイルの最大サイズ : {{ ServerinfoCloned.config.MESSAGE.FileMaxSize }}
           </p>
         </m-card>
 
@@ -37,7 +83,7 @@ const { getServerinfo } = storeToRefs(useServerinfo());
             ユーザー名の最大文字数 : {{ getServerinfo.config.PROFILE.usernameMaxLength }}
           </p>
           <p>
-            アイコン用画像ファイルの最大サイズ : {{ getServerinfo.config.PROFILE.iconMaxSize }}
+            アイコン用画像ファイルの最大サイズ : {{ ServerinfoCloned.config.PROFILE.iconMaxSize }}
           </p>
         </m-card>
       </div>
