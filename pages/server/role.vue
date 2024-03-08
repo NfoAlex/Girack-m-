b36f566ec1fc6e9e11dd3e55396f2d6237183962
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-08 15:40:54 +0900

    [add] - ロール管理ページの土台作成

--- pages/server/role.vue
+++ pages/server/role.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <NuxtLayout name="server">
-      <p>ロール管理</p>
+      <div class="d-flex flex-column" style="height:100%;">
+
+        <p class="text-h5">ロール管理</p>
+        <v-divider class="pb-0 mt-3" style="border-radius:8px;" thickness="3" />
+
+      </div>
     </NuxtLayout>
   </div>
 </template>
