3694b553f0f08e63deec28bb33dcdf69083098ed
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-10 00:06:00 +0900

    [change] - ディバイダをトップの区切りと区別させた

--- pages/server/role.vue
+++ pages/server/role.vue
@@ -54,7 +54,7 @@ onUnmounted(() => {
       <div class="d-flex flex-column" style="height:100%;">
 
         <p class="text-h5">ロール管理</p>
-        <v-divider class="pb-0 mt-3" style="border-radius:8px;" thickness="3" />
+        <v-divider class="pb-0 mt-3" thickness="3" />
 
         <p class="text-h6 py-2">ロールリスト</p>
         <div style="max-height:30vh; overflow-y:auto;" class="py-3">
@@ -67,7 +67,7 @@ onUnmounted(() => {
           </v-chip>
         </div>
 
-        <v-divider class="py-0 mb-3" style="border-radius:8px;" thickness="3" />
+        <v-divider class="py-0 mb-3" />
 
         <p class="text-h6 py-2">設定管理</p>
         <ServerRoleManager :roleEditing="roleEditing" />
