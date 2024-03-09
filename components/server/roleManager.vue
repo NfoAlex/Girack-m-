064b32631c8ac5b02126c5a8378d53274e8844c7
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-09 23:56:00 +0900

    [add] - ロール情報をチップで表示するように

---
+++ components/server/roleManager.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps(["roleEditing"]);
+</script>
+
+<template>
+  <div>
+    ここでロール管理
+    {{ props.roleEditing }}
+  </div>
+</template>
--- pages/server/role.vue
+++ pages/server/role.vue
@@ -34,13 +34,23 @@ onMounted(() => {
 
 <template>
   <div>
-    <NuxtLayout name="server">
+    <NuxtLayout name="server" style="height:100%;">
       <div class="d-flex flex-column" style="height:100%;">
 
         <p class="text-h5">ロール管理</p>
         <v-divider class="pb-0 mt-3" style="border-radius:8px;" thickness="3" />
 
-        {{ roleData }}
+        <div>
+          <p>ロールリスト</p>
+          <v-chip
+            v-for="role in roleData"
+            class="ma-1"
+          >
+            {{ role.name }}
+          </v-chip>
+        </div>
+
+        <v-divider class="pb-0 mt-3" style="border-radius:8px;" thickness="3" />
 
       </div>
     </NuxtLayout>
