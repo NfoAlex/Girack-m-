2b065680840514ed36254fea9a0c129f854fbc28
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-10 00:26:54 +0900

    [add] - propで渡されるロールデータの変更を監視して毎度クローンするように

--- components/server/roleManager.vue
+++ components/server/roleManager.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
-const props = defineProps(["roleEditing"]);
+import type role from '~/types/role';
+
+const props = defineProps<{
+  roleEditing: role
+}>();
+
+/**
+ * data
+ */
+const roleEditingClone = ref<role>({ //編集用のロールデータ
+  roleId: '',
+  name: '',
+  ServerManage: false,
+  RoleManage: false,
+  ChannelRename: false,
+  ChannelViewPrivate: false,
+  ChannelCreateAndDelete: false,
+  UserManage: false,
+  MessageDelete: false,
+  MessageAttatchFile: false
+});
+
+/**
+ * watch
+ */
+//propのロールデータの変更監視
+watch(props, () => {
+  //編集用ロール変数へロール情報をクローン
+  roleEditingClone.value = structuredClone(toRaw(props.roleEditing));
+
+  console.log("roleManager :: watch(roleEditing) : ", roleEditingClone.value);
+});
 </script>
 
 <template>
-  <div>
+  <div style="overflow-y: auto;">
     ここでロール管理
     {{ props.roleEditing }}
+
+    <p>ロール名</p>
+    <v-text-field v-model="roleEditingClone.name" />
+
   </div>
 </template>
