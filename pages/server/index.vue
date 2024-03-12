b243f1a3a6e70a72aed9c792c94ee9ba8d19ac12
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-12 21:23:44 +0900

    [change] - サーバー情報の受け取りと変更監視

--- pages/server/index.vue
+++ pages/server/index.vue
@@ -1,21 +1,49 @@
 <script setup lang="ts">
 import { socket } from '~/socketHandlers/socketInit';
-import { useServerinfo } from '~/stores/serverinfo';
+import { useMyUserinfo } from '~/stores/userinfo';
 
 import type { Serverinfo } from '~/types/serverInfo';
 
-const { getServerinfo } = storeToRefs(useServerinfo());
+const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
 
 /**
  * data
  */
+//情報が編集されたかどうか
+const stateEdited = ref<boolean>(false);
+//サーバー情報
+const ServerinfoFull = ref<Serverinfo>({
+  servername: '',
+  registration: {
+    available: false,
+    invite: {
+      inviteOnly: false,
+      inviteCode: ''
+    }
+  },
+  config: {
+    PROFILE: {
+      iconMaxSize: 0,
+      usernameMaxLength: 0
+    },
+    CHANNEL: {
+      channelIdAnnounceRegistration: '',
+      defaultJoinOnRegister: []
+    },
+    MESSAGE: {
+      TxtMaxLength: 0,
+      FileMaxSize: 0
+    }
+  }
+});
 //編集用サーバー情報変数
 const ServerinfoCloned = ref<Serverinfo>({
   servername: '',
   registration: {
     available: false,
     invite: {
-      inviteOnly: false
+      inviteOnly: false,
+      inviteCode: ''
     }
   },
   config: {
@@ -34,8 +62,63 @@ const ServerinfoCloned = ref<Serverinfo>({
   }
 });
 
+/**
+ * 編集用JSON変数を復元(クローンし直し)
+ */
+const restoreServerinfoCloned = () => {
+  //編集用JSON変数へクローン
+  ServerinfoCloned.value = structuredClone(toRaw(ServerinfoFull.value));
+  //編集状態を初期化
+  stateEdited.value = false;
+
+  //編集用JSONの変更を監視する
+  watch(ServerinfoCloned.value, () => {
+    if (JSON.stringify(ServerinfoCloned.value) === JSON.stringify(ServerinfoFull.value)) {
+      stateEdited.value = false;
+    } else {
+      stateEdited.value = true;
+    }
+  });
+}
+
+/**
+ * サーバー情報取得
+ * @param dat
+ */
+const SOCKETfetchServerInfoFull = (dat:{result:string, data:Serverinfo}) => {
+  console.log("/server :: SOCKETfetchServerInfoFull : dat->", dat);
+  ServerinfoFull.value = dat.data;
+
+  //編集用JSON変数へクローン
+  ServerinfoCloned.value = structuredClone(toRaw(dat.data));
+
+  //編集用JSONの変更を監視する
+  watch(ServerinfoCloned.value, () => {
+    if (JSON.stringify(ServerinfoCloned.value) === JSON.stringify(ServerinfoFull.value)) {
+      stateEdited.value = false;
+    } else {
+      stateEdited.value = true;
+    }
+  });
+}
+
+
+
 onMounted(() => {
-  ServerinfoCloned.value = structuredClone(toRaw(getServerinfo.value));
+  //サーバーFull情報受け取り用
+  socket.on("RESULT::fetchServerInfoFull", SOCKETfetchServerInfoFull);
+
+  //サーバー情報を全取得
+  socket.emit("fetchServerInfoFull", {
+    RequestSender: {
+      userId: getMyUserinfo.value.userId,
+      sessionId: getSessionId.value
+    }
+  });
+});
+
+onUnmounted(() => {
+  socket.off("RESULT::fetchServerInfoFull", SOCKETfetchServerInfoFull);
 });
 </script>
 
@@ -47,7 +130,66 @@ onMounted(() => {
         <v-divider class="pb-0 mt-3" style="border-radius:8px;" thickness="3" />
 
         <m-card class="my-3">
-          <v-text-field label="インスタンス名" variant="outlined" />
+          <p class="mb-2 text-h6">
+            {{ 
+              ServerinfoCloned.servername!==ServerinfoFull.servername
+              ?
+                '*インスタンス名'
+                :
+                'インスタンス名'
+            }}
+          </p>
+          <v-text-field
+            v-model="ServerinfoCloned.servername"
+            variant="outlined"
+            hide-details
+          />
+        </m-card>
+
+        <m-card class="my-3">
+          <p class="mb-2 text-h6">新規登録</p>
+
+          <v-switch
+            v-model="ServerinfoCloned.registration.available"
+            label="登録を有効化"
+          />
+
+          <v-divider class="my-5" />
+
+          <v-switch
+            v-model="ServerinfoCloned.registration.invite.inviteOnly"
+            :disabled="!ServerinfoCloned.registration.available"
+            label="新規登録を招待制にする"
+            class="my-3"
+          />
+
+          <v-text-field
+            v-model="ServerinfoCloned.registration.invite.inviteCode"
+            :disabled="
+              !ServerinfoCloned.registration.invite.inviteOnly
+              ||
+              !ServerinfoCloned.registration.available
+            "
+            :label="
+              ServerinfoCloned.registration.invite.inviteCode!==ServerinfoFull.registration.invite.inviteCode
+              ?
+                '*招待コード'
+                :
+                '招待コード'
+            "
+            variant="outlined"
+            hide-details
+          />
+        </m-card>
+
+        <m-card v-if="stateEdited" position="absolute" style="bottom:10px;">
+          <m-btn color="primary">変更を適用</m-btn>
+          <m-btn
+            @click="restoreServerinfoCloned"
+            variant="text"
+          >
+            復元
+          </m-btn>
         </m-card>
       </div>
     </NuxtLayout>
