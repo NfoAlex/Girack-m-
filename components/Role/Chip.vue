e130ae395481a1e6096f0d93838706307438eeed
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-16 03:58:38 +0900

    [add] - ロールchipのどっとにホバーするとxと表示するように

--- components/Role/Chip.vue
+++ components/Role/Chip.vue
@@ -6,15 +6,22 @@ const { getRoleSingle } = storeToRefs(useRole());
 const props = defineProps<{
   roleId: string
 }>();
+
+/**
+ * data
+ */
+const mouseHoveredOnDot = ref<boolean>(false);
 </script>
 
 <template>
   <v-chip size="small" class="mr-1 mt-1">
     <v-icon
+      @mouseover="mouseHoveredOnDot = true"
+      @mouseleave="mouseHoveredOnDot = false"
       :color="getRoleSingle(props.roleId).color"
       class="mr-2"
     >
-      mdi-circle
+      {{ mouseHoveredOnDot?"mdi-close-circle":"mdi-circle" }}
     </v-icon>
     {{ getRoleSingle(props.roleId).name }}
   </v-chip>
