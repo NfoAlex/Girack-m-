9d402a01865f99efb3da307f0192a40548b3990c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-23 19:39:03 +0900

    [add] - 状態監視処理インポートと簡易的Socket監視

--- app.vue
+++ app.vue
@@ -1,20 +1,30 @@
 <script setup>
+import { socket } from "./socketHandlers/socketInit";
 import { loadSocket } from "../socketHandlers/socketInit";
 import { useConfig } from "../stores/config";
 import { useAppStatus } from "../stores/AppStatus";
-
 
 //サービスの準備ができてからSocketの設定開始
 onNuxtReady(() => { loadSocket(); })
 
 //設定の値監視してテーマを切り替え
 const { getConfig } = storeToRefs(useConfig());
+//状態監視用
 const { getAppStatus } = storeToRefs(useAppStatus());
+
 //テーマ切り替え用
 const theme = useTheme();
+
 //監視開始
 watch(getConfig, async () => {
   theme.global.name.value = getConfig.value.theme; //テーマを設定で設定されているものへ設定
+});
+
+/**
+ * WSサーバーへの接続の監視、そしてAppStatusの更新
+ */
+socket.on("connected", () => {
+  getAppStatus.value.connected = true;
 });
 </script>
 
