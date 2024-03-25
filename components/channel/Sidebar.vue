7247a8b2060c6956d90acb7b5fd327179a4518e1
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-26 01:22:55 +0900

    [add] - サイドバーをボタンでチャンネルIDを表示、ページ移動できるように

--- components/channel/Sidebar.vue
+++ components/channel/Sidebar.vue
@@ -1,11 +1,42 @@
+<script setup lang="ts">
+import { useMyUserinfo } from '~/stores/userinfo';
+
+const { getMyUserinfo } = storeToRefs(useMyUserinfo());
+
+const router = useRouter();
+const route = useRoute();
+
+/**
+ * data
+ */
+const currentPath = ref<string>(""); //チャンネルID
+
+onMounted(() => {
+  //なぜかpathが配列ならブラウザへ
+  if (typeof(route.params.id) !== "object") {
+    currentPath.value = route.params.id;
+  } else {
+    router.push("/browser");
+  }
+});
+</script>
+
 <template>
   <div class="d-flex flex-column">
     チャンネルバー
     <div>
-      1
+      {{ $route.params.id }}
     </div>
-    <div>
-      2
-    </div>
+    <m-card-compact
+      v-for="channelId of getMyUserinfo.channelJoined"
+      @click="router.push('/channel/' + channelId)"
+      :variant="currentPath===channelId?'tonal':'text'"
+      :color="currentPath===channelId?'primary':null"
+      class="d-flex align-center px-3 py-2"
+      width="100%"
+    >
+      <v-icon class="mr-1" size="small">mdi-pound</v-icon>
+      <p>{{ channelId }}</p>
+    </m-card-compact>
   </div>
 </template>
