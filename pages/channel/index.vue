ecd72d393c332efc78a29cfd175cbd6c1d1f0766
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-26 00:50:49 +0900

    [add] - 会話ページのindexから自動的にチャンネルへ飛ぶように

--- pages/channel/index.vue
+++ pages/channel/index.vue
@@ -1,1 +1,19 @@
+<script setup lang="ts">
+import { useMyUserinfo } from '~/stores/userinfo';
+
+const { getMyUserinfo } = storeToRefs(useMyUserinfo());
+const router = useRouter();
+
+onMounted(() => {
+  //自分の参加チャンネル
+  const channelJoined:string[] = getMyUserinfo.value.channelJoined;
+  //最初のチャンネルへ移動
+  if (channelJoined.length !== 0) {
+    router.push("/channel/" + channelJoined[0]);
+  } else {
+    router.push("/browser");
+  }
+});
+
+</script>
 <template>ここチャンネルトップ</template>
