b7e12a1b42125026c0af10bec3778101b663efaa
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-26 01:56:05 +0900

    [change] - 縦padding調整

--- pages/channel/[id].vue
+++ pages/channel/[id].vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="px-3 py-4 d-flex align-stretch" style="height:100%;">
+  <div class="px-3 d-flex align-stretch" style="height:100%;">
     <ChannelSidebar class="mr-2" style="width:25%; max-width:250px;" />
-    <ChannelContainer class="px-2 flex-grow-1" />
+    <ChannelContainer class="px-2 py-2 flex-grow-1" />
   </div>
 </template>
