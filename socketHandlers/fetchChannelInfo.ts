1611d5a619f33c4d216c507d09b5f62e42c2f93d
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-26 23:29:14 +0900

    [add] - チャンネルデータを受け取った際Storeを更新するように

--- socketHandlers/fetchChannelInfo.ts
+++ socketHandlers/fetchChannelInfo.ts
@@ -2,13 +2,17 @@
 
 import { Socket } from "socket.io-client"; //クラス識別用
 import { useChannelinfo } from "~/stores/channel";
+import type { channel } from "~/types/channel";
 
 export default function fetchChannelInfo(socket: Socket): void {
   //チャンネル情報更新関数を取得
   const { updateChannelinfo } = useChannelinfo();
 
   //チャンネル情報
-  socket.on("RESULT::fetchChannelInfo", (dat) => {
-    console.log("infoChannel :: dat->", dat);
+  socket.on("RESULT::fetchChannelInfo", (dat:{result:string, data:channel}) => {
+    //console.log("infoChannel :: dat->", dat);
+
+    //チャンネル情報の更新
+    updateChannelinfo(dat.data.channelId, dat.data);
   });
 }
