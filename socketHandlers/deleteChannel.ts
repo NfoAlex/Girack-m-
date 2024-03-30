c7478ca5dc16f60b53fe84e25eb327fcf21962d4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-30 18:22:39 +0900

    [add/change] - チャンネル脱退結果受け取りハンドラ作成、参加チャンネルID削除処理を移動

--- socketHandlers/deleteChannel.ts
+++ socketHandlers/deleteChannel.ts
@@ -6,25 +6,9 @@ import { useMyUserinfo } from "~/stores/userinfo";
 
 export default function deleteChannel(socket: Socket): void {
   const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
-  //チャンネル情報更新関数を取得
-  const { deleteChannelinfo } = useChannelinfo();
-  const { updateMyUserinfo } = useMyUserinfo();
 
   //チャンネル削除通知
   socket.on("RESULT::deleteChannel", (dat:{result:string, data:string}) => {
-    //参加チャンネル配列を抽出
-    const channelJoined:string[] = getMyUserinfo.value.channelJoined;
-    //チャンネルIDを削除した配列を作成
-    const channelJoinedSpliced = channelJoined.splice(channelJoined.indexOf(dat.data), 1);
-    //チャンネル参加配列を更新させる
-    updateMyUserinfo({
-      ...getMyUserinfo.value,
-      channelJoined: channelJoinedSpliced
-    });
-    
-    //Storeからチャンネル情報を削除する
-    deleteChannelinfo(dat.data);
-
     //console.log("socket(deleteChannel) : 処理してる");
 
     //チャンネル脱退処理をする
---
+++ socketHandlers/leaveChannel.ts
@@ -0,0 +1,27 @@
+//チャンネル削除したっていう情報受け取り、処理
+
+import { Socket } from "socket.io-client"; //クラス識別用
+import { useChannelinfo } from "~/stores/channel";
+import { useMyUserinfo } from "~/stores/userinfo";
+
+export default function leaveChannel(socket: Socket): void {
+  const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+  //チャンネル情報更新関数を取得
+  const { updateMyUserinfo } = useMyUserinfo();
+
+  //チャンネル脱退結果
+  socket.on("RESULT::leaveChannel", (dat:{result:string, data:string}) => {
+    //成功ならチャンネル情報を削除する
+    if (dat.result === "SUCCESS") {
+      //参加チャンネル配列を抽出
+      const channelJoined:string[] = getMyUserinfo.value.channelJoined;
+      //チャンネルIDを削除した配列を作成
+      const channelJoinedSpliced = channelJoined.splice(channelJoined.indexOf(dat.data), 1);
+      //チャンネル参加配列を更新させる
+      updateMyUserinfo({
+        ...getMyUserinfo.value,
+        channelJoined: channelJoinedSpliced
+      });
+    }
+  });
+}
