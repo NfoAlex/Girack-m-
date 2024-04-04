a830f205e010d679dc1f238388f9e6012bed13ee
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-04 21:23:41 +0900

    [add] - コンテナにて履歴を参照、表示できるように

--- components/channel/Container/Content.vue
+++ components/channel/Container/Content.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import { useHistory } from '~/stores/history';
+import type { channel } from '~/types/channel';
+
+//props(チャンネル情報)
+const props = defineProps<{
+  channelInfo: channel
+}>();
+
+const { getHistoryFromChannel } = storeToRefs(useHistory());
+</script>
+
 <template>
   <div>
-    ここでしゃべる
+    {{
+    props.channelInfo.channelId!==undefined ?
+      getHistoryFromChannel(props.channelInfo.channelId) : "Loading..."
+    }}
   </div>
 </template>
