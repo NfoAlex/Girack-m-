df273408ab461ef377ea1fcf3932a0bf66b24cec
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-04 17:21:53 +0900

    [add] - InputにてEnterキーの処理の土台追加

--- components/channel/Container/Input.vue
+++ components/channel/Container/Input.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import { socket } from '~/socketHandlers/socketInit';
+
+/**
+ * data
+ */
+const messageInput = ref<string>("");
+
+/**
+ * Enterキーの処理
+ */
+const triggerEnter = () => {
+  console.log("/channel/:id :: Input : メッセージ->", messageInput.value);
+}
+</script>
+
 <template>
   <div>
-    <v-text-field variant="solo-filled" rounded></v-text-field>
+    <v-text-field
+      v-model="messageInput"
+      @keydown.enter="triggerEnter"
+      variant="solo-filled"
+      rounded
+    />
   </div>
 </template>
