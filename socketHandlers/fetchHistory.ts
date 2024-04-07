6c19dca6364c8f0e3a2d39df16e4fb4653fbc311
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-07 23:47:24 +0900

    [add] - 履歴の受け取り方法をバックエンドの仕様に合わせた

--- socketHandlers/fetchHistory.ts
+++ socketHandlers/fetchHistory.ts
@@ -6,11 +6,23 @@ import type message from "~/types/message";
 
 export default function fetchHistory(socket:Socket):void {
   //履歴データの受け取り
-  socket.on("RESULT::fetchHistory", (dat:{result:string, data:{channelId:string, history:message[]}}) => {
+  socket.on("RESULT::fetchHistory", (
+    dat: {
+      result:string,
+      data: {
+        channelId: string,
+        historyData: {
+          history: message[],
+          atTop: boolean,
+          atEnf: boolean
+        }
+      }
+    }
+  ) => {
     console.log("fetchHistory :: socketon(fetchHistory) : dat->", dat);
 
     //履歴をStoreへ格納
     const { setHistory } = useHistory();
-    setHistory(dat.data.channelId, dat.data.history);
+    setHistory(dat.data.channelId, dat.data.historyData.history);
   });
 }
