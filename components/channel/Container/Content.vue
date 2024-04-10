0130a0dbb834f284df613c79014a1ab4bb30475e
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-10 22:13:46 +0900

    [add] - スケルトンローダーの位置にスクロールしたら履歴をフェッチするように

--- components/channel/Container/Content.vue
+++ components/channel/Container/Content.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { socket } from '~/socketHandlers/socketInit';
 import { useHistory } from '~/stores/history';
+import { useMyUserinfo } from "~/stores/userinfo";
 import { useUserIndex } from "~/stores/userindex";
 import type { channel } from '~/types/channel';
 
+import { useElementVisibility } from '@vueuse/core'
+
+const { getMyUserinfo, getSessionId } = useMyUserinfo();
 const { getUserinfo } = useUserIndex();
 
 //props(チャンネル情報)
@@ -10,7 +15,63 @@ const props = defineProps<{
   channelInfo: channel
 }>();
 
-const { getHistoryFromChannel, getHistoryAvailability } = storeToRefs(useHistory());
+const { getHistoryFromChannel, getHistoryAvailability } = useHistory();
+
+/**
+ * data
+ */
+const skeletonLoader = ref(null); //要素位置監視用
+const atSkeleton = useElementVisibility(skeletonLoader); //スケルトンローダーが画面内にあるかどうか
+
+/**
+ * 古い履歴の追加取得
+ */
+const fetchOlderHistory = () => {
+  //履歴の長さカウント
+  const lengthOfHistory = getHistoryFromChannel(props.channelInfo.channelId).length;
+  //一番古いメッセージID
+  const oldestMessageId = getHistoryFromChannel(
+    props.channelInfo.channelId
+  )[lengthOfHistory-1].messageId;
+  
+  console.log("/channel/:id :: fetchOlderHistory : oldestMessageId->", oldestMessageId);
+
+  //履歴を取得
+  socket.emit("fetchHistory", {
+    RequestSender: {
+      userId: getMyUserinfo.userId,
+      sessionId: getSessionId
+    },
+    channelId: props.channelInfo.channelId,
+    fetchingPosition: {
+      positionMessageId: oldestMessageId,
+      fetchDirection: "older"
+    }
+  });
+
+  console.log("/channel/:id :: fetchOlderHistory : 送信したもの->", {
+    RequestSender: {
+      userId: getMyUserinfo.userId,
+      sessionId: getSessionId
+    },
+    channelId: props.channelInfo.channelId,
+    fetchingPosition: {
+      positionMessageId: oldestMessageId,
+      fetchDirection: "older"
+    }
+  });
+
+}
+
+//スケルトンローダーの位置変数の監視
+watch(atSkeleton, function (newValue, oldValue) {
+  console.log("/channel/:id :: watch(atSkeleton) : atSkeleton->", newValue, oldValue);
+
+  //もしスケルトンローダーの位置にいるのなら履歴を追加で取得
+  if (newValue) {
+    fetchOlderHistory();
+  }
+});
 </script>
 
 <template>
@@ -38,6 +99,7 @@ const { getHistoryFromChannel, getHistoryAvailability } = storeToRefs(useHistory
       <!-- 戦闘のロードホルダー -->
       <span
         v-if="!getHistoryAvailability(props.channelInfo.channelId).atTop"
+        ref="skeletonLoader"
       >
         <v-skeleton-loader type="list-item-avatar" color="background"></v-skeleton-loader>
         <v-skeleton-loader type="list-item-avatar" color="background"></v-skeleton-loader>
