c8410599ae0b17a4a55c6b757f0e70001901db30
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-14 20:27:08 +0900

    [add] - 絵文字ピッカーを表示できるように

--- components/channel/Container/Content/MessageRender/HoverMenu.vue
+++ components/channel/Container/Content/MessageRender/HoverMenu.vue
@@ -1,22 +1,47 @@
+<script setup lang="ts">
+import { Picker, EmojiIndex } from 'emoji-mart-vue-fast/src';
+import data from "emoji-mart-vue-fast/data/twitter.json";
+import "emoji-mart-vue-fast/css/emoji-mart.css";
+
+//絵文字ピッカー用データ
+let emojiIndex = new EmojiIndex(data);
+
+/**
+ * data
+ */
+const displayEmojiPicker = ref<boolean>(false);
+
+</script>
+
 <template>
-  <v-card
-    class="pa-1"
-    rounded="pill"
-    height="fit-content"
-  >
-    <v-btn
-      @click=""
-      size="small"
-      variant="text"
-      icon="mdi-emoticon-happy-outline"
-      class="pa-2"
+  <span>
+    <Picker
+      v-if="displayEmojiPicker"
+      :data="emojiIndex"
+      title="リアクション"
+      style="position:absolute; bottom:15px; right:100%;"
+      :defaultSkin="1"
     />
-    <v-btn
-      @click=""
-      size="small"
-      variant="text"
-      icon="mdi-pencil"
-      class="pa-2"
-    />
-  </v-card>
+
+    <v-card
+      class="pa-1"
+      rounded="pill"
+      height="fit-content"
+    >
+      <v-btn
+        @click="displayEmojiPicker = !displayEmojiPicker;"
+        size="small"
+        :variant="displayEmojiPicker?'tonal':'text'"
+        icon="mdi-emoticon-happy-outline"
+        class="pa-2"
+      />
+      <v-btn
+        @click=""
+        size="small"
+        variant="text"
+        icon="mdi-pencil"
+        class="pa-2"
+      />
+    </v-card>
+  </span>
 </template>
