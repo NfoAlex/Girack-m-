1fcefe58134da49b624b517dc2774f1b0b593e1e
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-15 23:43:06 +0900

    [add] - 既読状態取得状況をAppStatusへ追加

--- pages/channel/[id].vue
+++ pages/channel/[id].vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+import { useAppStatus } from "~/stores/AppStatus";
+//AppStatus取得用
+const { getAppStatus } = storeToRefs(useAppStatus());
+</script>
+
 <template>
   <div class="px-4 pt-4 d-flex align-stretch" style="height:100%;">
     <ChannelChannelbar class="mr-2 flex-shrink-0" style="width:20vw; max-width:250px;" />
-    <ChannelContainer class="px-2 py-2 flex-shrink-1 flex-grow-1" />
+    <ChannelContainer v-if="getAppStatus.hasMessageReadId" class="px-2 py-2 flex-shrink-1 flex-grow-1" />
+    <div v-else>Loading...</div>
   </div>
 </template>
