c5e56c504e8cd9e9937ca6ada5ea290ee6e46199
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-16 00:07:40 +0900

    [add] - 忘れてた

---
+++ socketHandlers/getMessageReadId.ts
@@ -0,0 +1,23 @@
+//単一ロール情報受け取り用
+
+import { Socket } from "socket.io-client"; //クラス識別用
+import { useAppStatus } from "~/stores/AppStatus";
+import { useMessageReadId } from "~/stores/messageReadId";
+
+export default function getMessageReadId(socket: Socket): void {
+  //サーバー(インスタンス)情報
+  socket.on("RESULT::getMessageReadId", (dat:{
+    result: string,
+    data: any
+  }) => {
+    console.log("getMessageReadId :: dat->", dat);
+
+   const { setMessageReadId } = useMessageReadId();
+   const { getAppStatus } = storeToRefs(useAppStatus());
+
+   //最新のメッセージ既読Idを格納
+   setMessageReadId(dat.data);
+    //最新既読Idを持っていると設定
+   getAppStatus.value.hasMessageReadId = true;
+  });
+}
