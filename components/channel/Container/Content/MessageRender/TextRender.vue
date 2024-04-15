305d7faea07df2184073ffe2ca7f68fdff7a8df4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-15 12:15:17 +0900

    [add] - TextRenderコンポを作成、インポート

---
+++ components/channel/Container/Content/MessageRender/TextRender.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  content: string
+}>();
+</script>
+
+<template>
+  <p class="text-medium-emphasis" style="word-break: break-all;">
+    {{ props.content }}
+  </p>
+</template>
