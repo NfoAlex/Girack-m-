440a6288d8391d4baf2d8a50cc7edefc73690b1c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-16 18:48:17 +0900

    [add] - メッセージ来た時そのチャンネルにいて、かつ一番下にいるなら自動で最新既読Idを更新

--- socketHandlers/receiveMessage.ts
+++ socketHandlers/receiveMessage.ts
@@ -2,6 +2,8 @@
 
 import { Socket } from "socket.io-client"; //クラス識別用
 import { useHistory } from "~/stores/history";
+import { useMessageReadId } from "~/stores/messageReadId";
+import { useMyUserinfo } from "~/stores/userinfo";
 import type message from "~/types/message";
 
 export default function receiveMessage(socket: Socket): void {
@@ -11,5 +13,29 @@ export default function receiveMessage(socket: Socket): void {
     //console.log("socket(receiveMessage) :: message->", message);
 
     addMessage(message);
+
+    //現在そのチャンネルにいて、一番下にスクロールしているのなら最新既読Id更新
+    const route = useRoute();
+    if (typeof(route.params.id) !== "object") {
+      if (route.params.id === message.channelId) {
+        //最終既読IdをStoreにて更新
+        const { updateMessageReadId } = useMessageReadId();
+        updateMessageReadId(
+          message.channelId,
+          message.messageId
+        );
+
+        //メッセージ既読状態を設定する
+        const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+        socket.emit("setMessageReadId", {
+          RequestSender: {
+            userId: getMyUserinfo.value.userId,
+            sessionId: getSessionId.value
+          },
+          channelId: message.channelId,
+          messageId: message.messageId
+        });
+      }
+    }
   });
 }
