6bc2b58f306d0cbdd177f437c7ea76b3d81208ca
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-04-18 11:24:03 +0900

    [add] - 最新既読Idに一つ古いId用のホルダー作成

--- stores/messageReadId.ts
+++ stores/messageReadId.ts
@@ -27,6 +27,10 @@ export const useMessageReadId = defineStore("messagereadid", {
     getMessageReadId: (state) => (channelId:string) => {
       return state._MessageReadId[channelId];
     },
+
+    getMessageReadIdBefore: (state) => (channelId:string) => {
+      return state._MessageReadIdBefore[channelId];
+    },
   },
   
   actions: {
@@ -39,6 +43,13 @@ export const useMessageReadId = defineStore("messagereadid", {
     //チャンネルを指定して設定
     updateMessageReadId(channelId:string, messageId:string) {
       //console.log("messageReadTime :: setmessageReadTime : 今->", this._MessageReadId);
+      //もし一つ古い最新既読IdがundefinedならそのメッセージIDに
+      if (this._MessageReadIdBefore[channelId] === undefined) {
+        this._MessageReadIdBefore[channelId] = messageId;
+      } else {
+        this._MessageReadIdBefore[channelId] = this._MessageReadId[channelId];
+      }
+      //最新既読Idを更新
       this._MessageReadId[channelId] = messageId;
     },
   }
