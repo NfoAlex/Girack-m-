339fc86d78f991c0de36bbb7f69060bcd6abae64
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-29 17:26:08 +0900

    [add] - メッセ削除用Socketハンドラ作成

---
+++ socketHandlers/deleteMessage.ts
@@ -0,0 +1,24 @@
+//メッセージの削除
+
+import { Socket } from "socket.io-client"; //クラス識別用
+import { useHistory } from "~/stores/history";
+
+export default function deleteMessage(socket: Socket): void {
+  const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+
+  //メッセ削除
+  socket.on("RESULT::deleteMessage", (
+    dat: {
+      result: string,
+      data: {
+        channelId: string,
+        messageId: string
+      }
+    }) => {
+    //console.log("socket(deleteChannel) : 処理してる");
+
+    //削除する
+    const { deleteMessage } = useHistory();
+    deleteMessage(dat.data.channelId, dat.data.messageId);
+  });
+}
