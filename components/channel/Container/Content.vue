16544be6a927ed351b62ec748c6b397c76620dd0
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-05-10 00:04:00 +0900

    [remove] - 不要

--- components/channel/Container/Content.vue
+++ components/channel/Container/Content.vue
@@ -368,21 +368,7 @@ watch(props, (newProp, oldProp) => {
         offset: 10
         }
       );
-    } /*else {
-      //このチャンネルの履歴長取得
-      const historyLength = getHistoryFromChannel(props.channelInfo.channelId).length;
-      //空じゃないならスクロール
-      if (historyLength !== 0) {
-        const oldestMessageId = getHistoryFromChannel(props.channelInfo.channelId)[historyLength-1];
-        goTo(
-          "#msg" + oldestMessageId,
-          {
-            duration: 0,
-            container: "#ChannelContainerContent",
-          }
-        );
-      }
-    } */
+    }
 
     //もし履歴の最後にいるなら新着を消す
     if (getHistoryAvailability(props.channelInfo.channelId).atEnd) {
