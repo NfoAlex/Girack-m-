a72c291060e2113e126e8977961ac8c0a075e510
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-14 12:46:08 +0900

    [change] - Welcome内のテキスト編集改善

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -6,8 +6,9 @@ const page = ref<number>(1);
 <template>
   <v-dialog width="55vw" max-width="550px" height="65vh" max-height="750px">
     <m-card height="100%" width="100%" class="pa-7">
-      <v-card-body class="text-center pt-5">
-        <h2>Girackへようこそ!</h2>
+      <v-card-body class="pt-5">
+        <h2 class="mb-3 text-center">Girackへようこそ!</h2>
+        <li>しゃべろう！</li>
       </v-card-body>
       <v-card-actions class="mt-auto d-flex flex-row-reverse">
         <m-btn color="primary">次へ</m-btn>
