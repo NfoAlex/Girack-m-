6f61e4224417ea5b8de1621df38cdd52fd0e476d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-15 10:08:22 +0900

    [add] - オンラインユーザー表示部分を独立させた

---
+++ components/index/OnlineUsers.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mt-12">
+      <h4>オンラインユーザー</h4>
+      <v-chip size="large" variant="flat">
+        <v-avatar class="mr-2" size="x-small">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          ></v-img>
+        </v-avatar>
+        <p>ユーザー</p>
+      </v-chip>
+    </div>
+</template>
