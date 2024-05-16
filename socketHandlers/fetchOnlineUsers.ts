61962cee503f1c0e926364304f6bb2680c4a0a10
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-16 10:20:35 +0900

    [fix] - ハンドラ名

--- socketHandlers/fetchOnlineUsers.ts
+++ socketHandlers/fetchOnlineUsers.ts
@@ -3,7 +3,7 @@
 import { Socket } from "socket.io-client"; //クラス識別用
 
 export default function fetchOnlineUsers(socket: Socket): void {
-  socket.on("fetchOnlineUsers", (dat:{result:string, data:any[]}) => {
+  socket.on("RESULT::fetchOnlineUsers", (dat:{result:string, data:any[]}) => {
     console.log("socket(fetchOnlineUsers) :: dat->", dat);
   });
 }
