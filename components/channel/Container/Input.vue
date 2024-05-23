a982c57848eddf330ec35f87420b983a43082aeb
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-23 10:38:32 +0900

    [add] - ユーザー、チャンネル検索用データ設定追加

--- components/channel/Container/Input.vue
+++ components/channel/Container/Input.vue
@@ -10,10 +10,30 @@ const props = defineProps<{
   channelInfo: channel
 }>();
 
+//メンションデータ用interface
+interface SearchData {
+  query: string, //検索文字列
+  searching: boolean, //検索モードに入っているかどうか
+  selectedIndex: number, //選択しているもの
+  searchStartingAt: number, //検索モードに入った文字位置
+  searchEndingAt: number, //検索文字列の範囲終わり(文字列全体の長さ - searchStartingAt)
+  txtLengthWhenStartSearching: number, //検索をし始めたときの文字列全体の長さ
+  searchingTerm: "user"|"channel", //ToDo::(!現在未使用!)検索するもの("user" | "channel")
+};
+
 /**
  * data
  */
-const messageInput = ref<string>("");
+const messageInput = ref<string>(""); //メッセージ入力用変数
+const searchData = ref<SearchData>({ //検索データ
+  query: '',
+  searching: false,
+  selectedIndex: 0,
+  searchStartingAt: 0,
+  searchEndingAt: 0,
+  txtLengthWhenStartSearching: 0,
+  searchingTerm: 'user'
+});
 
 /**
  * Enterキーの処理
