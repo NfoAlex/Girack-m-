721625956c4d88f4bd5f660b9a74a989a050cc00
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-04 16:10:03 +0900

    [add] - プレビューデータ用のinterface更新

--- types/message.ts
+++ types/message.ts
@@ -3,6 +3,24 @@ export default interface message {
   channelId: string,
   userId: string,
   content: string,
+  linkData: {
+    [key: string]:
+      {
+        contentType: "text/html",
+        mediaType: string,
+        url: string,
+        siteName: string,
+        description: string,
+        images: string[]
+      }
+        |
+      {
+        mediaType: "image"|"video",
+        url: string,
+        contentType: string,
+        favicons: string[]
+      }
+  }|{},
   time: string,
   reaction: {
     [key: string]: {
