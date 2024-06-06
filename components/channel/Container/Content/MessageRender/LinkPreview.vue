3200e2a35c8ab83216da2a2304dac782061a1ef4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-06-06 19:08:45 +0900

    [add] - リンクプレビューデータ用のコンポ作成

---
+++ components/channel/Container/Content/MessageRender/LinkPreview.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type message from '~/types/message';
+
+const props = defineProps<{linkData: message["linkData"]}>();
+
+/**
+ * リンクプレビューデータJSONのキーを配列にしたものを返す
+ */
+const linkDataKeyArr = computed(():string[] => {
+  //リンクデータがnullなら空
+  if (props.linkData === null) return [];
+  return Object.keys(props.linkData);
+});
+</script>
+
+<template>
+  <span
+    v-for="index of linkDataKeyArr"
+    v-if="props.linkData!==null"
+  >
+    <!-- 通常ウェブサイト用 -->
+    <span v-if="props.linkData[index].contentType==='text/html'">
+      {{ props.linkData[index].siteName }}
+    </span>
+
+    <!-- 画像用 -->
+    <span v-if="props.linkData[index].mediaType==='image'">
+      {{ props.linkData[index].url }}
+    </span>
+  </span>
+</template>
