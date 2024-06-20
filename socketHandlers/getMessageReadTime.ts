c7b7b352ae593fe491044386111bbd636289d014
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-21 05:05:28 +0900

    [add] - 最新既読時間受け取り用Socketハンドラ作成

---
+++ socketHandlers/getMessageReadTime.ts
@@ -0,0 +1,47 @@
+//最新既読時間受信用
+
+import { Socket } from "socket.io-client"; //クラス識別用
+import { useAppStatus } from "~/stores/AppStatus";
+import { useMyUserinfo } from "~/stores/userinfo";
+import { useMessageReadTime } from "~/stores/messageReadTime";
+
+export default function getMessageReadTime(socket: Socket): void {
+  socket.on("RESULT::getMessageReadTime", (dat:{
+    result: string,
+    data: {
+      [key: string]: string
+    }
+  }) => {
+    console.log("getMessageReadTime :: dat->", dat);
+
+   const { setMessageReadTime } = useMessageReadTime();
+   const { getAppStatus } = storeToRefs(useAppStatus());
+
+   //最新のメッセージ既読時間を格納
+   setMessageReadTime(dat.data);
+
+   //もし初回の受信なら履歴を取得
+   if (!getAppStatus.value.hasMessageReadId) {
+     //参加しているチャンネルの数分ループして取得
+    const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+    for (let channelId of getMyUserinfo.value.channelJoined) {
+      //履歴を取得
+      socket.emit("fetchHistory", {
+        RequestSender: {
+          userId: getMyUserinfo.value.userId,
+          sessionId: getSessionId.value
+        },
+        channelId: channelId,
+        fetchingPosition: {
+          positionMessageId: dat.data[channelId]===undefined?'':dat.data[channelId],
+          includeThisPosition: true,
+          fetchDirection: "older"
+        }
+      });
+    }
+   }
+
+    //最新既読Idを持っていると設定
+   getAppStatus.value.hasMessageReadId = true;
+  });
+}
