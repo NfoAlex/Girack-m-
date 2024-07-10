00464be742bcc9b6f50c92d00932e4a2b8d7473d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-07-11 01:05:48 +0900

    [add] - 通知設定ページを作成

---
+++ pages/settings/notification.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useConfig } from '~/stores/config';
+
+const { getConfig } = storeToRefs(useConfig());
+</script>
+
+<template>
+  <div>
+    <NuxtLayout name="settings">
+      <p class="text-h6 mb-6">通知</p>
+      <m-card>
+        <v-switch
+          v-model="getConfig.notification.notifyAllMessages"
+          label="受信するすべてのメッセージを通知する"
+        />
+
+        <v-switch
+          v-model="getConfig.notification.notifyMention"
+          label="受信した自分宛てのメンションメッセージを通知する"
+        />
+
+      </m-card>
+    </NuxtLayout>
+  </div>
+</template>
