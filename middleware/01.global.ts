2ac7d969b52147d41b9f2b8b1001954d47d983dc
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-07-16 18:39:28 +0900

    [add] - ファイル閲覧用ページを認証確認処理から除外

--- middleware/01.global.ts
+++ middleware/01.global.ts
@@ -1,10 +1,19 @@
 import { useAppStatus } from "~/stores/AppStatus";
 
-export default defineNuxtRouteMiddleware(to => {
+export default defineNuxtRouteMiddleware((to) => {
   const nuxtApp = useNuxtApp();
   if (process.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered) return;
 
+
   //ログインが済んでいないなら認証画面へ飛ばす
   const { getAppStatus } = storeToRefs(useAppStatus());
-  if (to.path !== "/auth" && !getAppStatus.value.profile.authDone) return navigateTo('/auth');
+  if (
+    (
+      to.path !== "/auth"
+      &&
+      !to.path.startsWith("/file")
+    )
+    &&
+    !getAppStatus.value.profile.authDone
+  ) return navigateTo('/auth');
 });
