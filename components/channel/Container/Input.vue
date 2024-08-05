cbcbc3fc55e42f63993e964e26cb0a2b988791e2
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-05 12:12:39 +0900

    [fix] - メンション時の無駄な改行を無くした (#129)

--- components/channel/Container/Input.vue
+++ components/channel/Container/Input.vue
@@ -372,11 +372,14 @@ const triggerDown = (e: Event) => {
 const insertResult = (targetId: string) => {
   //入力テキストの@部分をメンション文で代入
   if (searchData.value.query === "") {
-    messageInput.value = `
-        ${messageInput.value.substring(0, searchData.value.searchStartingAt)}
-        @<${targetId}> 
-        ${messageInput.value.substring(searchData.value.searchStartingAt + 1)}
-      `;
+    // biome-ignore lint/style/useTemplate: Biomeが${}を使わせようとするけどそれだと無駄な改行が入る
+    const messageResult = messageInput.value.substring(0, searchData.value.searchStartingAt)
+        + `@<${targetId}>`
+        + messageInput.value.substring(searchData.value.searchStartingAt + 1);
+        
+    //console.log("Input :: insertResult : 挿入しようとしている文字列->", messageResult);
+
+    messageInput.value = messageResult;
   } else {
     messageInput.value = messageInput.value.replace(
       `@${searchData.value.query}`,
