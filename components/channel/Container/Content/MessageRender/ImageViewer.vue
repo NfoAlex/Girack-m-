c8f3c6ee1dc5076e9a6ddf0b2b7a0b37c3035692
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-06 17:42:03 +0900

    [add] - 画像ビューワーの土台コンポ作成

---
+++ components/channel/Container/Content/MessageRender/ImageViewer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  imageUrls: string[]
+}>();
+</script>
+
+<template>
+  <v-dialog>
+    <div>
+
+    </div>
+
+    <m-card>
+      asdf
+    </m-card>
+  </v-dialog>
+</template>
