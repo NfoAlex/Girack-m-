52162cf8d5079706b62a603b6d53bca73f8f3430
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-07 17:26:20 +0900

    [add] - 画像ビューワーで拡大表示をできるように
    まだ単体のみ

--- components/channel/Container/Content/MessageRender/ImageViewer.vue
+++ components/channel/Container/Content/MessageRender/ImageViewer.vue
@@ -2,15 +2,33 @@
 const props = defineProps<{
   imageUrls: string[]
 }>();
+
+const emits = defineEmits<(e: "closeDialog") => void>();
+
+/**
+ * data
+ */
+const activeImageUrl = ref<string>(props.imageUrls[0]);
+
+onMounted(() => {
+  console.log("ImageViewer :: マウントされた");
+});
 </script>
 
 <template>
-  <v-dialog>
-    <div>
-
+  <v-dialog height="85vh" class="d-flex flex-column align-center justify-center">
+    <div
+      @click="emits('closeDialog')"
+      class="flex-grow-1 d-flex align-center justify-center"
+      style="height:max-content;"
+    >
+      <v-img
+        @click.stop="console.log('ここ画像')"
+        :src="activeImageUrl"
+      />
     </div>
 
-    <m-card>
+    <m-card v-if="props.imageUrls.length!==1" class="mt-auto mx-auto" maxWidth="650" width="100%">
       asdf
     </m-card>
   </v-dialog>
