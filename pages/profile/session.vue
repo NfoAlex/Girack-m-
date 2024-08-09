4e09e234f4b40bdd62c04568ebc2fc39895403c7
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-09 17:30:37 +0900

    [add] - セッションページの土台整備

--- pages/profile/session.vue
+++ pages/profile/session.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import { useMyUserinfo } from '~/stores/userinfo';
+const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+
+definePageMeta(
+  {key: route => route.fullPath}
+);
+</script>
+
 <template>
-  
+  <div>
+    <NuxtLayout name="profile">
+      <div class="pa-4">
+      </div>
+    </NuxtLayout>
+  </div>
 </template>
