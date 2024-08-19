b7fbe985addddbfc26f322ea0e76acb9cca288e9
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-19 16:32:03 +0900

    [change] - 画像プレビュー単体コンポのスタイル調整

--- components/file/ImagePreview.vue
+++ components/file/ImagePreview.vue
@@ -110,6 +110,8 @@ onBeforeMount(() => {
     <NuxtImg
       :src="getImageUrl(props.fileId)"
       quality="75"
+      class="rounded-lg py-1 px-1"
+      style="max-width:64px; max-height:64px;"
       loading="lazy"
     />
   </div>
