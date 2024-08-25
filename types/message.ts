7338320ccd592a0b42231a9d2342b5424445598c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-25 18:52:39 +0900

    [add] - システムメッセージ用のInterface追加

--- types/message.ts
+++ types/message.ts
@@ -39,3 +39,21 @@ export interface inbox {
     [key: string]: string[]; //ここでのkeyはチャンネルId
   };
 }
+
+//システムメッセージの内容用フラッグ
+export type ISystemMessageFlag =
+  "SERVER_JOINED" |
+  "SERVER_UPDATED" |
+  "CHANNEL_INVITED" |
+  "CHANNEL_JOINED" |
+  "CHANNEL_LEFT" |
+  "CHANNEL_KICKED" |
+  "CHANNEL_INFO_UPDATED"
+;
+
+//システムメッセージの内容用型
+export interface ISystemMessageContent {
+  flag: ISystemMessageFlag,
+  targetUserId: string | null,
+  senderUserId: string
+}
