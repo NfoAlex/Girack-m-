37f32081b261203246e0ea99da08adb0e7d2f037
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-26 12:33:27 +0900

    System message (#139)
    * [add] - システムメッセージ用のInterface追加
    
    * [add] - システムメッセージ状態のboolをInterfaceへ追加
    
    * [add] - システムメッセージのJSONを表示する部分を作成
    
    あと一番下メッセ用refの作り方が無駄なやり方だった
    
    * [change] - システムメッセJSONを文章で表示するように
    
    * [add] - ユーザーId部分でチップを表示できるように
    
    * [add] - システムメッセ用辞書更新
    
    * [add] - ユーザーチップ表示にテキスト省略を適用
    
    * [change] - ユーザーチップのスタイル調整
    
    * [change] - システムメッセの表示の省略改善
    
    * Biome Check

---
+++ components/channel/Container/Content/SystemMessageRender.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import type {
+  ISystemMessageContent,
+  ISystemMessageFlag,
+} from "~/types/message";
+import UserChip from "./MessageRender/TextRender/UserChip.vue";
+
+const props = defineProps<{
+  content: string;
+}>();
+
+//システムメッセージ文表示用
+const flagDictionary = new Map<ISystemMessageFlag, string>([
+  ["CHANNEL_JOINED", "がチャンネルへ参加しました!"],
+  ["CHANNEL_INVITED", "がチャンネルへ招待されました"],
+  ["CHANNEL_LEFT", "がチャンネルから退出しました。"],
+  ["CHANNEL_INFO_UPDATED", "がチャンネル情報を更新しました。"],
+  ["CHANNEL_KICKED", "がチャンネルからキックされました。"],
+  ["SERVER_JOINED", "がサーバーへ参加しました!"],
+  ["SERVER_UPDATED", "がサーバー情報を更新しました。"],
+]);
+
+/**
+ * data
+ */
+const systemMessageJson = ref<ISystemMessageContent>({
+  flag: "SERVER_JOINED",
+  targetUserId: null,
+  senderUserId: "",
+});
+
+onMounted(() => {
+  try {
+    //パースして格納してみる
+    systemMessageJson.value = JSON.parse(props.content);
+  } catch (e) {}
+});
+</script>
+
+<template>
+  <div
+    v-if="systemMessageJson.senderUserId !== ''"
+    class="d-flex align-center justify-center text-medium-emphasis mx-auto my-3"
+    style="width:100%;"
+  >
+    <UserChip :userId="systemMessageJson.senderUserId" style="min-width:25%" class="mr-1 text-truncate flex-shrink-0" />
+    <p class="text-truncate">{{ flagDictionary.get(systemMessageJson.flag) }}</p>
+  </div>
+</template>
--- types/message.ts
+++ types/message.ts
@@ -3,6 +3,7 @@ export default interface message {
   channelId: string;
   userId: string;
   isEdited: boolean;
+  isSystemMessage: boolean;
   content: string;
   linkData: {
     [key: string]:
@@ -38,4 +39,21 @@ export interface inbox {
   event: {
     [key: string]: string[]; //ここでのkeyはチャンネルId
   };
+}
+
+//システムメッセージの内容用フラッグ
+export type ISystemMessageFlag =
+  | "SERVER_JOINED"
+  | "SERVER_UPDATED"
+  | "CHANNEL_INVITED"
+  | "CHANNEL_JOINED"
+  | "CHANNEL_LEFT"
+  | "CHANNEL_KICKED"
+  | "CHANNEL_INFO_UPDATED";
+
+//システムメッセージの内容用型
+export interface ISystemMessageContent {
+  flag: ISystemMessageFlag;
+  targetUserId: string | null;
+  senderUserId: string;
 }
