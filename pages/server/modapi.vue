1bf32ecc1da941395466a1ecf9481799b28ce05d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-28 05:10:30 +0900

    [add/change] - API利用者情報表示の整備と再取得ボタンの設定

--- pages/server/modapi.vue
+++ pages/server/modapi.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import { socket } from "~/socketHandlers/socketInit";
 import { useMyUserinfo } from "~/stores/userinfo";
+import { useUserIndex } from "~/stores/userindex";
 import type { IAPIClientInfo } from "~/types/api";
 
 const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+const { getUserinfo } = storeToRefs(useUserIndex());
 
 /**
  * data
  */
 const APIInfos = ref<IAPIClientInfo[]>([]);
+
+/**
+ * API情報取得
+ */
+const fetchAllApiInfo = () => {
+  socket.emit("fetchAllApiInfo", {
+    RequestSender: {
+      userId: getMyUserinfo.value.userId,
+      sessionId: getSessionId.value
+    }
+  });
+}
 
 /**
  * API利用情報を取得
@@ -25,13 +39,7 @@ const SOCKETfetchAllApiInfo = (dat:{result:string, data:IAPIClientInfo[] | null}
 onMounted(() => {
   socket.on("RESULT::fetchAllApiInfo", SOCKETfetchAllApiInfo);
 
-  //API情報取得
-  socket.emit("fetchAllApiInfo", {
-    RequestSender: {
-      userId: getMyUserinfo.value.userId,
-      sessionId: getSessionId.value
-    }
-  });
+  fetchAllApiInfo();
 });
 
 onUnmounted(() => {
@@ -42,9 +50,31 @@ onUnmounted(() => {
 <template>
   <div style="height:100%;">
     <NuxtLayout name="server" style="height:100%;">
-      ここでAPI管理
+      <div class="d-flex flex-row mb-2">
+        <v-btn
+          @click="fetchAllApiInfo"
+          color="primary"
+          rounded="lg"
+          class="ml-auto"
+          icon="mdi-refresh"
+        />
+      </div>
       <m-card v-for="apiClient,index of APIInfos">
-        {{ apiClient }}
+        <div class="d-flex flex-row align-center">
+          <v-icon
+            size="x-small"
+            class="mr-2"
+            :color="apiClient.isEnabled ? 'success' : 'error'"
+          >mdi-circle</v-icon>
+          <p>{{ apiClient.clientName }}</p>
+          <v-divider vertical class="mx-2" />
+          <p>{{ apiClient.description }}</p>
+        </div>
+        <v-divider class="my-2" />
+        <div class="d-flex flex-row">
+          <v-chip class="mr-2">最新利用日時 : {{ apiClient.latestUsedTime || "未利用" }}</v-chip>
+          <v-chip>{{ getUserinfo(apiClient.createdUserId).userName }}</v-chip>
+        </div>
       </m-card>
     </NuxtLayout>
   </div>
