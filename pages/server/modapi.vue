649e193a5e2aaa8e2fcd10eaf4d4211bd309b35e
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-28 04:40:09 +0900

    [add] - API管理者用画面で情報を取得、表示だけできるように

--- pages/server/modapi.vue
+++ pages/server/modapi.vue
@@ -1,7 +1,51 @@
+<script setup lang="ts">
+import { socket } from "~/socketHandlers/socketInit";
+import { useMyUserinfo } from "~/stores/userinfo";
+import type { IAPIClientInfo } from "~/types/api";
+
+const { getMyUserinfo, getSessionId } = storeToRefs(useMyUserinfo());
+
+/**
+ * data
+ */
+const APIInfos = ref<IAPIClientInfo[]>([]);
+
+/**
+ * API利用情報を取得
+ * @param dat
+ */
+const SOCKETfetchAllApiInfo = (dat:{result:string, data:IAPIClientInfo[] | null}) => {
+  console.log("modapi :: SOCKETfetchAllApiInfo : dat->", dat);
+
+  if (dat.data) {
+    APIInfos.value = dat.data;
+  }
+}
+
+onMounted(() => {
+  socket.on("RESULT::fetchAllApiInfo", SOCKETfetchAllApiInfo);
+
+  //API情報取得
+  socket.emit("fetchAllApiInfo", {
+    RequestSender: {
+      userId: getMyUserinfo.value.userId,
+      sessionId: getSessionId.value
+    }
+  });
+});
+
+onUnmounted(() => {
+  socket.off("fetchAllApiInfo", SOCKETfetchAllApiInfo);
+});
+</script>
+
 <template>
   <div style="height:100%;">
     <NuxtLayout name="server" style="height:100%;">
       ここでAPI管理
+      <m-card v-for="apiClient,index of APIInfos">
+        {{ apiClient }}
+      </m-card>
     </NuxtLayout>
   </div>
 </template>
